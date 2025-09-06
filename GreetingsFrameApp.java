382ccc73ec9c8fdf032a35e0ba8ec67ef449c485
Mthuthuzeli Paul Ndwendwa <102731341+gridX777@users.noreply.github.com>
2025-09-06 02:52:05 -0700

    Add files via upload

---
+++ GreetingsFrameApp.java
@@ -0,0 +1,10 @@
+package greetingsframeapp;
+import za.ac.tut.ui.GreetingsGUI;
+
+public class GreetingsFrameApp {
+
+    public static void main(String[] args) {
+        new GreetingsGUI();
+    }
+    
+}
---
+++ GymMembershipFrameApp.java
@@ -0,0 +1,11 @@
+package gymmembershipframeapp;
+import za.ac.tut.ui.*;
+        
+public class GymMembershipFrameApp {
+
+  
+    public static void main(String[] args) {
+        new GymMembershipFrame();
+    }
+    
+}
